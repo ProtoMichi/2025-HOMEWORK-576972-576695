053e02063eb8ff2ef8ed251ac518ef196f8d056f
Michele Carta <128220574+ProtoMichi@users.noreply.github.com>
2025-03-23 16:11:26 +0100

    Aggiunti e Organizzati i Sorgenti in Package
    Aggiunti e Organizzati i Sorgenti in Package verificare se tutto ok e funzionante

---
+++ src/it/uniroma3/diadia/Partita.java
@@ -0,0 +1,93 @@
+package it.uniroma3.diadia;
+
+import it.uniroma3.diadia.ambienti.Labirinto;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.giocatore.Giocatore;
+
+/**
+ * Questa classe modella una partita del gioco
+ *
+ * @author  docente di POO
+ * @see Stanza
+ * @version base
+ */
+
+public class Partita {
+
+	private Stanza stanzaCorrente;
+	private Stanza stanzaVincente;
+	private Labirinto labirinto;
+	private Giocatore giocatore;
+	private boolean finita;
+
+	public Partita(){
+		this.labirinto=new Labirinto();
+		this.giocatore=new Giocatore();
+		this.finita = false;
+		this.stanzaVincente = this.labirinto.getUscita();
+		this.stanzaCorrente = this.labirinto.getEntrata();
+	}
+
+
+
+	public Labirinto getLabirinto() {
+		return labirinto;
+	}
+
+
+
+
+	public void setLabirinto(Labirinto labirinto) {
+		this.labirinto = labirinto;
+	}
+
+
+	public Stanza getStanzaVincente() {
+		return stanzaVincente;
+	}
+
+	public void setStanzaCorrente(Stanza stanzaCorrente) {
+		this.stanzaCorrente = stanzaCorrente;
+	}
+
+	public Stanza getStanzaCorrente() {
+		return this.stanzaCorrente;
+	}
+
+	/**
+	 * Restituisce vero se e solo se la partita e' stata vinta
+	 * @return vero se partita vinta
+	 */
+	public boolean vinta() {
+		return this.getStanzaCorrente()== this.getStanzaVincente();
+	}
+
+	/**
+	 * Restituisce vero se e solo se la partita e' finita
+	 * @return vero se partita finita
+	 */
+	public boolean isFinita() {
+		return finita || vinta() || (this.giocatore.getCfu() == 0);
+	}
+
+	/**
+	 * Imposta la partita come finita
+	 *
+	 */
+	public void setFinita() {
+		this.finita = true;
+	}
+
+	public Giocatore getGiocatore() {
+		return giocatore;
+	}
+
+
+
+
+	public void setGiocatore(Giocatore giocatore) {
+		this.giocatore = giocatore;
+	}
+
+
+}
